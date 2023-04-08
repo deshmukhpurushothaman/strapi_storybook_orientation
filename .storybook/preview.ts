43e641f9cfad1381ecf5ce001e59ba174c8ea853
Deshmukh P <deshmukhpurushothaman@gmail.com>
2023-04-08 13:12:35 +0530

    🔼image configuration in storybook and button component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,18 @@
 import type { Preview } from "@storybook/react";
+import * as NextImage from 'next/image'
+import React from 'react';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => React.createElement(OriginalNextImage, { ...props, unoptimized: true }),
+});
+
+// Object.defineProperty(NextImage, '__esModule', {
+//   configurable: true,
+//   value: true,
+// });
 
 const preview: Preview = {
   parameters: {
